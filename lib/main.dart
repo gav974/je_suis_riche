9508aa71298bc281fa5480f41408f99fafc3e9ae
gav974ordi1 <eclapier@msn.com>
2022-01-04 13:42:43 +0400

    phase 1 du projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
 
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Je Suis Riche',
+      title: "Je Suis Riche",
+       theme: ThemeData( fontFamily: 'Raleway'),
+
       home:Scaffold(
         appBar: AppBar(
-          title:Text('Je suis Riche '),
-      ),
-        body: Center(
-          child: Image.asset('asset/diamond.jpg'),
-        ),
+          title:const Text("Je suis Riche "),
+          backgroundColor: Colors.white10,
 
+        ),
+        backgroundColor: Colors.black,
+
+        body:Center(
+          child: FittedBox(
+            child:Image.asset('asset/diamond.jpg'),
+            fit: BoxFit.fill,
+
+        ),
+        ),
       )
     );
   }
